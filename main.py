a06e613d400412f185b1035c6e9995503d6c5b02
Liron Avraham <lironavr@post.bgu.ac.il>
2019-07-02 16:10:49 +0300

    update

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import pickle
 import time
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import colors
+from matplotlib.ticker import PercentFormatter
 from joblib import dump, load
 from sklearn.decomposition import PCA
 
@@ -126,38 +128,101 @@ class Data:
         #    self._scans.append(scan)
 
 
+def createGraph(scan, title):
+    x = []
+    for i in range(1, 361):
+        x.append(i)
+    plt.plot(x, scan.distances, label="Distance")
+    plt.plot(x, scan.intensities, label="Intensity")
+    plt.xlabel("Angle")
+    plt.ylabel("Value")
+    plt.title(title + " LDS Scan")
+    plt.legend()
+    plt.show()
+
+
+def create_two_scan_graph(benign, malicious, title):
+    x = []
+    for i in range(1, 361):
+        x.append(i)
+    plt.plot(x, benign['distances'], label="Benign")
+    plt.plot(x, malicious['distances'], label="Malicious")
+    #plt.plot(x, benign.intensities, label="Intensity")
+    plt.xlabel("Angle")
+    plt.ylabel("Value")
+    plt.title(title + " Compared LDS Scan")
+    plt.legend()
+    plt.show()
+
+
+def create_average_arrays(data):
+    avg_dist = np.zeros(360)
+    avg_int = np.zeros(360)
+    #avg_err = np.zeros(360)
+    for i in data._scans:
+        for j in range(0, 360):
+            avg_dist[j] += i.distances[j]
+            #if(i.distances[j] > 2500):
+            #    print("Angle is: " + str(j) + " and the distance is: " + str(i.distances[j]))
+            avg_int[j] += i.intensities[j]
+            #avg_err[j] += i.errors[j]
+    for i in range(0,360):
+        avg_dist[i] = avg_dist[i]/len(data._scans)
+        avg_int[i] = avg_int[i]/len(data._scans)
+        #avg_err[i] = avg_err[i]/data._scans.size
+    ans={}
+    ans['distances'] = avg_dist
+    ans['intensities'] = avg_int
+    return ans
+
+
 def train():
     data = Data()
-    data.read_data('benign_p1')
+    #data.read_data('benign_p1')
     data.read_data('benign_p2')
-    data.read_data('benign_p3')
-    data.read_data('benign_p4')
-    data.read_data('benign_p5')
+    #data.read_data('benign_p3')
+    #data.read_data('benign_p4')
+    #data.read_data('benign_p5')
     train_x = []
     train_y = []
+    #####
+    #createGraph(data._scans[10], 'Benign')
+    #####
     for i in data._scans:
         for j in range(0, 360):
             train_x.append([i.distances[j], i.errors[j], i.intensities[j]])
             train_y.append(1)
     data1 = Data()
-    data1.read_data('mal_p1_1')
+    #data1.read_data('mal_p1_1')
     data1.read_data('mal_p2')
-    data1.read_data('mal_p3')
-    data1.read_data('mal_p4')
-    data1.read_data('mal_p5')
+    #data1.read_data('mal_p3')
+    #data1.read_data('mal_p4')
+    #data1.read_data('mal_p5')
+    ########
+    #createGraph(data1._scans[10], 'Malicious')
+    avg_benign = create_average_arrays(data)
+    avg_malicious = create_average_arrays(data1)
+    create_two_scan_graph(avg_benign, avg_malicious, 'Average')
+    ########
     for i in data1._scans:
         for j in range(0, 360):
             train_x.append([i.distances[j], i.errors[j], i.intensities[j]])
             train_y.append(0)
     X = np.array(train_x)
+    #print(X)
     pca = PCA()
-    x_pca = pca.fit_transform(X)
+
+    ###############
+    pca.fit_transform(train_x)
+    print(pca.explained_variance_)
+    ###############
     y = np.array(train_y)
 
-    clf = RandomForestClassifier(max_depth=100, random_state=0)  # max_depth is set max
+    clf = RandomForestClassifier(max_depth=360, random_state=0)  # max_depth is set max
     clf.fit(X, y)
     print("Accuracy when training: " + str(clf.score(X, y)))
     return clf
+
 
 def classify(scan, train_result):
     data_x = []
@@ -212,7 +277,7 @@ if __name__ == "__main__":
     #         correct += 1
     #
     data1 = Data()
-    data1.read_data('mal_p2')
+    data1.read_data('mal_p4')
     for i in data1._scans:
         total += 1
         result = classify(i, train_result)
